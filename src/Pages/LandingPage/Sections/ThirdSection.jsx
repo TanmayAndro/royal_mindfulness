3404b009d3d569f44cd2a302cb4f852d90d529bb
tanmayAndro <tanmayAndro@gmail.com>
2025-01-02 12:27:57 +0530

    fixed Video scetion

--- src/Pages/LandingPage/Sections/ThirdSection.jsx
+++ src/Pages/LandingPage/Sections/ThirdSection.jsx
@@ -2,23 +2,29 @@ import React from "react";
 import videoImg from "../../../Assests/VideoImg.png";
 import { Box } from "@mui/material";
 
-
 export const ThirdSection = () => {
   return (
     <Box
       sx={{
-        width:'100%',
-        height: "630px",
-        backgroundImage: `url(${videoImg})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
+        width: "100%",
+        height: { xs: "300px", sm: "100%", md: "100%" }, // Responsive height
         position: "relative",
         cursor: "pointer",
+        backgroundColor: "#0F2E15",
       }}
       onClick={() => {
         window.open("https://www.youtube.com/watch?v=ssss7V1_eyA", "_blank");
       }}
-    ></Box>
+    >
+      <img
+        src={videoImg}
+        alt="Video Thumbnail"
+        style={{
+          width: "100%",
+          height: "100%", // Optional: Set height to 100% to fill the Box
+          // objectFit: "cover", // Ensures the image covers the Box without distortion
+        }}
+      />
+    </Box>
   );
 };
